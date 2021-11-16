43a3b59308ef42046057b7a0931e28a19ccea731
Gabriel Custodio Oliveira <37151013+Gabslocked@users.noreply.github.com>
2021-11-16 00:13:46 -0300

    php login

---
+++ pages/index.php
@@ -0,0 +1,231 @@
+Gabslocked
+#3632
+
+Gabslocked — 07/12/2020
+VC É CRIENTE?
+VC É CRIENTE?
+https://garticphone.com/en/?c=503c65931e
+Gartic Phone - The Telephone Game
+The online Telephone Game! Play now the popular game for free
+
+entra ai
+Gabslocked — 13/12/2020
+cola ai
+Gabslocked
+ iniciou uma chamada que durou 4 minutos.
+ — 19/12/2020
+Gabslocked — 19/12/2020
+aff
+sem mic
+aff
+vou cagar e tomar banho
+comprei o pc novo hoje
+Vitrola — 19/12/2020
+boa
+Gabslocked — 20/12/2020
+df
+Gabslocked
+ iniciou uma chamada que durou poucos segundos.
+ — 20/12/2020
+Gabslocked
+ iniciou uma chamada que durou 5 minutos.
+ — 20/12/2020
+Gabslocked
+ iniciou uma chamada que durou 5 minutos.
+ — 20/12/2020
+Gabslocked
+ iniciou uma chamada que durou poucos segundos.
+ — 20/12/2020
+Gabslocked
+ iniciou uma chamada que durou poucos segundos.
+ — 22/06/2021
+Gabslocked — 22/06/2021
+a
+Vitrola — 22/06/2021
+o disc caiu
+Gabslocked
+ iniciou uma chamada que durou poucos segundos.
+ — 22/06/2021
+Gabslocked — 15/09/2021
+https://discord.gg/GPBYwRz9
+Gabslocked — 15/09/2021
+a.
+https://www.vultr.com/?ref=8923113-6G
+Vultr
+SSD VPS Servers, Cloud Servers and Cloud Hosting by Vultr
+Vultr Global Cloud Hosting - Brilliantly Fast SSD VPS Cloud Servers. 100% KVM Virtualization
+
+Gabslocked — 15/09/2021
+https://discord.gg/5pymcdpu
+Gabslocked — 17/10/2021
+https://meet.google.com/pce-iffi-jwd?authuser=1
+Meet
+Real-time meetings by Google. Using your browser, share your video, desktop, and presentations with teammates and customers.
+Imagem
+Gabslocked
+ iniciou uma chamada que durou poucos segundos.
+ — 17/10/2021
+Vitrola — 17/10/2021
+$(enviar).click();
+Gabslocked — 19/10/2021
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+a
+aaa
+a
+Gabslocked
+ iniciou uma chamada que durou poucos segundos.
+ — 19/10/2021
+Gabslocked — Hoje às 13:41
+https://prod.liveshare.vsengsaas.visualstudio.com/join?58FE78E324EAACF3878C99AB335109EC24B5
+Visual Studio Code for the Web
+Build with Visual Studio Code, anywhere, anytime, entirely in your browser.
+Gabslocked — Hoje às 14:20
+201.27.90.77
+Gabslocked — Hoje às 14:36
+https://www.zerotier.com/
+ZeroTier
+Adam Ierymenko
+Home Main
+Vitrola — Hoje às 14:49
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+Expandir
+message.txt
+8 KB
+﻿
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>APS - Login</title>
+
+        <!-- Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="../css/main.css"/>
+    </head>
+<body>
+    <div class="wrap wrap-login">
+        <div class="menu">
+            <div class="logo">
+                <svg width="63" height="46" viewBox="0 0 63 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="31.2146" width="20.9098" height="20.9098" transform="rotate(45 31.2146 0)" fill="white"/>
+                    <rect x="47.6437" y="16.4291" width="20.9098" height="20.9098" transform="rotate(45 47.6437 16.4291)" fill="white"/>
+                    <rect x="14.7854" y="16.4291" width="20.9098" height="20.9098" transform="rotate(45 14.7854 16.4291)" fill="white"/>
+                </svg>
+            </div>
+        </div>
+        <div class="login-box">
+            <h4>COMECE AGORA</h4>
+
+            <!-- Crie sua conta -->
+            <div class="create-wrap">
+                <h1>CRIE SUA CONTA</h1>
+                <p>Já possui uma conta? <span onclick="toggleLogin();">Faça seu login</span></p>
+
+                <!-- Form Sign up -->
+                <form method="POST" action="config/signup.php">
+                    <div class="login-box-form">
+                        <div class="input-box">
+                            <input type="text" class="custom-input"/>
+                            <label>Nome</label>
+                        </div>
+                        <div class="input-box">
+                            <input type="text" class="custom-input"/>
+                            <label>Sobrenome</label>
+                        </div>
+                        <div class="input-box fullwidth">
+                            <input type="text" class="custom-input" id="txtCriarEmail"/>
+                            <label>Email</label>
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M22 6L12 13L2 6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+                        <div class="input-box fullwidth">
+                            <input type="password" class="custom-input"/>
+                            <label>Senha</label>
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12C23 12 19 20 12 20C5 20 1 12 1 12Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>                        
+                        </div>
+                        <div class="input-box fullwidth">
+                            <input type="password" class="custom-input"/>
+                            <label>Confirme sua senha</label>
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12C23 12 19 20 12 20C5 20 1 12 1 12Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>                        
+                        </div>
+                    </div>
+                    <button type="button" class="btn btn-primary" onclick="createAccount();" title="Criar conta">Criar conta</button>
+                </form>
+            </div>
+
+            <!-- Login -->
+            <div class="login-wrap hidden" style="opacity: 0;">
+                <h1>FAÇA SEU LOGIN</h1>
+                <p>Ainda nao possui uma conta? <span onclick="toggleLogin();">Crie sua conta</span></p>
+
+                <!-- Form Login -->
+                <form method="POST" action="config/login.php">
+                        <div class="login-box-form">
+                        <div class="input-box fullwidth">
+                            <input id="txtEmail" name="email" type="text" class="custom-input"/>
+                            <label>Email</label>
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M22 6L12 13L2 6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+                        <div class="input-box fullwidth">
+                            <input id="txtPassword" name="password" type="password" class="custom-input"/>
+                            <label>Senha</label>
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12C23 12 19 20 12 20C5 20 1 12 1 12Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>                        
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary" title="Fazer login">Fazer login</button>
+                </form>
+            </div>
+        </div>
+        <div class="bottom-links">
+            <a href="sobre.html">Quem somos</a>
+        </div>
+    </div>
+    
+    <!-- Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="../js/funcoes.js"></script>
+</body>
+</html>
+message.txt
+8 KB
